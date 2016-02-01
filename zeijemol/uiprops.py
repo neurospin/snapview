d57a4dfb69e9af9458b66c02dc6f82223f0767d8
Antoine Grigis <antoine.grigis@cea.fr>
2016-02-01 16:46:07 +0100

    uiprops: add local ref to bootstrap template: http url not supported in https.

--- zeijemol/uiprops.py
+++ zeijemol/uiprops.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# NSAp - Copyright (C) CEA, 2013
+# NSAp - Copyright (C) CEA, 2016
 # Distributed under the terms of the CeCILL-B license, as published by
 # the CEA-CNRS-INRIA. Refer to the LICENSE file or to
 # http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
@@ -8,7 +8,7 @@
 
 
 STYLESHEETS += [
-    "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
+    data("bootstrap/bootstrap_3_3_6.min.css"),
     data("cubes.snapview.css")
 ]
 
@@ -19,5 +19,5 @@ STYLESHEETS_PRINT += [
 
 JAVASCRIPTS += [
     "https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js",
-    "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js",
+    data("bootstrap/bootstrap_3_3_6.min.js")
 ]
