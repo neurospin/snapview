86ba1810a83c0392fc8eb1de9ef15f6d3336a92a
Antoine Grigis <antoine.grigis@cea.fr>
2016-08-05 13:44:58 +0200

    zeijemol/views: update the views in order to deal with the new schema and update to rate controller and status pages.

--- zeijemol/views/gallery.py
+++ zeijemol/views/gallery.py
@@ -9,19 +9,23 @@
 # System import
 from __future__ import division
 import base64
-import random
 import json
 import os
+import numpy
+from numpy.random import choice
 
 # CW import
 from cgi import parse_qs
 from cubicweb.view import View
+# TODO: change
+import cubes.snapview as zeijemol
 
 
 class Gallery(View):
     """ Custom view to score snap files.
     """
     __regid__ = "gallery-view"
+    extra_answers_description = u"Justify rating"
 
     def call(self, **kwargs):
         """ Create the rate form.
@@ -34,32 +38,62 @@ class Gallery(View):
         title = kwargs["title"][0]
         wave_name = kwargs["wave"][0]
 
-        # Get the snap to rate
-        self.w(u'<h1>{0}</h1>'.format(title))
+        # Get the wave extra answers
         rset = self._cw.execute("Any E Where W is Wave, W name '{0}', "
                                 "W extra_answers E".format(wave_name))
         extra_answers = json.loads(rset[0][0])
-        rset = self._cw.execute("Any S Where W is Wave, W name '{0}', "
-                                "W snaps S".format(wave_name))
-        rset_indices = []
-        for index in range(rset.rowcount):
-            snap_entity = rset.get_entity(index, 0)
-            scores = [
-                e for e in snap_entity.scores
-                if e.scored_by[0].login == self._cw.session.login]
-            if len(scores) == 0:
-                rset_indices.append(index)
-        if len(rset_indices) == 0:
-            error = "No more snap to rate, thanks."
-            self.w(u"<p class='label label-danger'>{0}</p>".format(error))
-            return
-        nb_of_snaps = rset.rowcount
-        nb_snaps_to_rate = len(rset_indices)
-        rand_index = random.randint(0, nb_snaps_to_rate - 1)
-        snap_entity = rset.get_entity(rset_indices[rand_index], 0)
+
+        # Select the snapset to be rated: use the internal connection in order
+        # to get the full rating distribution and then intersect this
+        # distribution with the user rates in order to uniformally
+        # fill the database
+        # Note: need to access the selected snapset with the user rights
+        # because the internal session is closed after with ...:
+        with self._cw.cnx._cnx.repo.internal_cnx() as cnx:
+            rset = cnx.execute("Any S Where W is Wave, W name '{0}', "
+                               "W snapsets S".format(wave_name))
+            # > find the user ratings and the all user ratings distribution
+            nb_scores = []
+            nb_user_scores = []
+            for index in range(rset.rowcount):
+                entity = rset.get_entity(index, 0)
+                scores = entity.scores
+                nb_scores.append(len(scores))
+                nb_user_scores.append(
+                    len([sc for sc in scores
+                        if sc.scored_by[0].login == self._cw.session.login]))
+            nb_scores = numpy.asarray(nb_scores).astype(numpy.single)
+            nb_user_scores = numpy.asarray(nb_user_scores)
+            # > check that the user has something to rate
+            if nb_user_scores.min() == 1:
+                error = "No more snap to rate, thanks."
+                self.w(u"<h1>{0}</h1>".format(error))
+                return
+            # > compute a sampling distribution
+            # p(snapset|len(snapset_scores)=X) = 1/(X + 1) (before norm)
+            # p(snapset) = 1./(X * Nx) where Nx= number of snapsets with
+            # X scores (rated X times)
+            keys, counts = numpy.unique(nb_scores, return_counts=True)
+            item_per_score = dict(zip(keys, counts))
+            weights = [1. / ((x + 1) * item_per_score[x]) for x in nb_scores]
+            weights = numpy.asarray(weights).astype(numpy.single)
+            # > set a null probability for already rated snapsets
+            weights[numpy.where(nb_user_scores == 1)] = 0
+            # > normalize the distribution
+            weights = weights / sum(weights)
+            # > weighted pick
+            snapset_index = choice(range(len(weights)), p=weights)
+            snapset_eid = rset[snapset_index][0]
+        rset = self._cw.execute("Any S Where S eid '{0}'".format(snapset_eid))
+        snapset_entity = rset.get_entity(0, 0)
+
+        # Display title
+        self.w(u'<h1>{0}</h1>'.format(title))
 
         # Dispaly status
-        progress = int((1 - nb_snaps_to_rate / nb_of_snaps) * 100)
+        nb_of_snapsets = len(nb_scores)
+        nb_snapsets_to_rate = numpy.sum(1 - nb_user_scores)
+        progress = int((1 - nb_snapsets_to_rate / nb_of_snapsets) * 100)
         self.w(u'<div class="progress">')
         self.w(u'<div class="progress-bar" role="progressbar" '
                'aria-valuenow="{0}" aria-valuemin="0" aria-valuemax='
@@ -68,52 +102,109 @@ class Gallery(View):
         self.w(u'</div>')
         self.w(u"</div>")
 
-        # Display the image to rate
-        if snap_entity.dtype == "CTM":
-            self.w(u'<div id="gallery-img">')
-            href = self._cw.data_url("qcsurf/population_mean_sd.json")
-            fsdir = os.path.join(os.path.dirname(snap_entity.filepath),
-                                    os.pardir)
-            self.wview(
-                "mesh-qcsurf", None, "null", fsdir=fsdir,
-                header=[snap_entity.code], populationpath=href)
-            self.w(u'</div>')
-        else:
-            with open(snap_entity.filepath, "rb") as image_file:
-                encoded_string = base64.b64encode(image_file.read())
-            self.w(u'<div id="gallery-img">')
-            if snap_entity.dtype.lower() == "pdf":
-                self.w(u'<embed class="gallery-pdf" alt="Embedded PDF" '
-                       'src="data:application/pdf;base64, {0}" />'.format(
-                            encoded_string))
-            else:
-                self.w(u'<img class="gallery-img" alt="Embedded Image" '
-                       'src="data:image/{0};base64, {1}" />'.format(
-                           snap_entity.dtype.lower(), encoded_string))
-            self.w(u'</div>')
-
-        # Display/Send a form
-        href = self._cw.build_url("rate-controller", eid=snap_entity.eid,
-                                  filepath=snap_entity.filepath)
-        self.w(u'<div id="gallery-form">')
-        self.w(u'<form action="{0}" method="post">'.format(href))
-        self.w(u'<input class="btn btn-success" type="submit" '
-               'name="rate" value="Good"/>')
-        self.w(u'<input class="btn btn-danger" type="submit" '
-               'name="rate" value="Bad"/>')
-        self.w(u'<input class="btn btn-info" type="submit" '
-               'name="rate" value="Rate later"/>')
+        # Display/send a form
+        href = self._cw.build_url("rate-controller", eid=snapset_entity.eid,
+                                  wave_name=wave_name)
+        wave_entity = snapset_entity.wave[0]
+        score_definitions = json.loads(wave_entity.score_definitions)
+        form_html = []
+        form_html.append(u'<div id="gallery-form">')
+        form_html.append(u'<form action="{0}" method="post">'.format(href))
+        for definition in score_definitions:
+            form_html.append(
+                u'<input class="btn btn-success triview-btn" type="submit" '
+                 'name="rate" value="{0}"/>'.format(definition))
+        form_html.append(
+            u'<input class="btn btn-info triview-btn" type="submit" '
+             'name="rate" value="Rate later"/>')
         if len(extra_answers) > 0:
-            self.w(u'<u>Optional observations:</u>')
+            form_html.append(u'<br/><br/>')
+            form_html.append(u'<u>{0}:</u>'.format(
+                self.extra_answers_description))
         for extra in extra_answers:
-            self.w(u'<div class="checkbox">')
-            self.w(u'<label>')
-            self.w(u'<input class="checkbox" type="checkbox" '
-                   'name="extra_answers" value="{0}"/>'.format(extra))
-            self.w(unicode(extra))
-            self.w(u'</label>')
-            self.w(u'</div>')
-        self.w(u'</form>')
-        self.w(u'</div>')
+            form_html.append(u'<div class="checkbox">')
+            form_html.append(u'<label>')
+            form_html.append(u'<input class="checkbox" type="checkbox" '
+                              'name="extra_answers" value="{0}"/>'.format(
+                                    extra))
+            form_html.append(unicode(extra))
+            form_html.append(u'</label>')
+            form_html.append(u'</div>')
+        form_html.append(u'</form>')
+        form_html.append(u'</div>')
 
-        self.w(u'<div id="floating-clear"/>')
+        # Check that all the viewers are declared
+        in_error = False
+        for snap_entity in snapset_entity.snaps:
+            if snap_entity.viewer not in ("FILE", "SURF", "TRIPLANAR"):
+                error = ("Can't find the appropriate viewer. Please contact "
+                         "the service administrator specifying the '{0}' "
+                         "wave viewer '{1}' is not responding "
+                         "on '{2}'.".format(wave_entity.name,
+                                            snap_entity.viewer,
+                                            self._cw.base_url()))
+                self.w(u"<h1>{0}</h1>".format(error))
+                in_error = True
+
+        # Display all the declared snaps for this snapset
+        if not in_error:
+            # > render form
+            self.w("\n".join(form_html))
+            for snap_entity in snapset_entity.snaps:
+                # > get external files
+                filepaths = [e.filepath for e in snap_entity.files]
+                # > display the files
+                if snap_entity.viewer == "FILE":
+                    if len(filepaths) != 1:
+                        raise ValueError(
+                            "Fatal Error: check system integrity "
+                            "'{0}'.".format(snap_entity.identifier))
+                    dtype = snap_entity.files[0].dtype
+                    with open(filepaths[0], "rb") as image_file:
+                        encoded_string = base64.b64encode(image_file.read())
+                    self.w(u'<div id="gallery-img">')
+                    if dtype.lower() == "pdf":
+                        self.w(
+                            u'<embed class="gallery-pdf" alt="Embedded PDF" '
+                             'src="data:application/pdf;base64, '
+                             '{0}" />'.format(encoded_string))
+                    else:
+                        self.w(
+                            u'<img class="gallery-img" alt="Embedded Image" '
+                             'src="data:image/{0};base64, {1}" />'.format(
+                                   dtype.lower(), encoded_string))
+                    self.w(u'</div>')
+                # > display the files in a triplanar view
+                elif snap_entity.viewer == "TRIPLANAR":
+                    file_data = {}
+                    for e in snap_entity.files:
+                        file_data.setdefault(e.description, []).append(
+                            (e.order, e.filepath, e.dtype))
+                    data_type = None
+                    for key in file_data.keys():
+                        if data_type is None:
+                            data_type = file_data[key][0][2]
+                        file_data[key] = [
+                            elem[1] for elem in sorted(file_data[key],
+                                                       key=lambda x: x[0])]
+                    self.w(u'<div id="gallery-triplanar" class="leftblock">')
+                    self.wview("triplanar-view", None, 'null',
+                               snap_entity=snap_entity, file_data=file_data,
+                               data_type=data_type)
+                    self.w(u'</div>')
+                # > display the surfaces
+                elif snap_entity.viewer == "SURF":
+                    self.w(u'<div id="gallery-img">')
+                    json_stats = self._cw.vreg.config["json_population_stats"]
+                    if not os.path.isfile(json_stats):
+                        json_stats = os.path.join(
+                            os.path.dirname(zeijemol.__file__), "data",
+                            "qcsurf", "population_mean_sd_default.json")
+                    self.wview("mesh-qcsurf", None, "null",
+                               filepaths=filepaths,
+                               header=[snapset_entity.name],
+                               populationpath=json_stats)
+                    self.w(u'</div>')
+
+            # Clear float
+            self.w(u'<div id="floating-clear"/>')
